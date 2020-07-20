3d118667b71d8334b668cf712abd9e00cb609746
hijuliancode <hijuliansosa@protonmail.com>
2020-07-20 14:32:31 -0500

    Stateful vs Stateless: App how Stateful component (this.state)

--- src/components/App.js
+++ src/components/App.js
@@ -1,36 +1,50 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const App = () => {
-  
-  const setRows = () => [
-    <tr>
-      <td>Meraki</td>
-      <td>hi@meraki.com</td>
-      <td>meraki.com</td>
-    </tr>,
-    <tr>
-      <td>Julian</td>
-      <td>hi@julian.com</td>
-      <td>julian.com</td>
-    </tr>
-  ]
-
-  return (
-    <div className="margin">
-      <table>
-        <thead>
-          <tr>
-            <th>Nombre</th>
-            <th>Correo</th>
-            <th>Enlace</th>
-          </tr>
-        </thead>
-        <tbody>
-          { setRows() }
-        </tbody>
-      </table>
-    </div>
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      users: [
+        {
+          name: 'Meraki',
+          email: 'hi@meraki.com',
+          link: 'meraki.com',
+        },
+        {
+          name: 'Julian',
+          email: 'hi@julian.com',
+          link: 'julian.com',
+        },
+      ]
+    }
+  }
+  setRows = () => (
+    this.state.users.map(user => (
+       <tr>
+         <td>{user.name}</td>
+         <td>{user.email}</td>
+         <td>{user.link}</td>
+       </tr>
+    ))
   )
+  render () {
+    return (
+      <div className="margin">
+        <table>
+          <thead>
+            <tr>
+              <th>Nombre</th>
+              <th>Correo</th>
+              <th>Enlace</th>
+            </tr>
+          </thead>
+          <tbody>
+            { this.setRows() }
+          </tbody>
+        </table>
+      </div>
+    )
+  }
 }
 
 export default App
