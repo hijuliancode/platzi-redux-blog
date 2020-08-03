96d64eb14a0d51c9ef4299b3079b2b91caea316f
hijuliancode <hijuliansosa@gmail.com>
2020-08-03 09:52:35 -0500

    Try Catch

--- src/redux/actions/usersActions.js
+++ src/redux/actions/usersActions.js
@@ -2,10 +2,15 @@ import axios from 'axios'
 import { GET_USERS } from '../../constants/usersTypes'
 
 export const getUsers = () => async (dispatch) => {
-  const resp = await axios.get('https://jsonplaceholder.typicode.com/users')
+  try {
+    const resp = await axios.get('https://jsonplaceholder.typicode.com/userss')
 
-  dispatch({
-    type: GET_USERS,
-    payload: resp.data
-  })
+    dispatch({
+      type: GET_USERS,
+      payload: resp.data
+    })
+  }
+  catch (error) {
+    console.log('Error =>', error.message)
+  }
 }
